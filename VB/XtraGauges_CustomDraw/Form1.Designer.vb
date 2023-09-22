826f5d00aec13a0902b27b68d10bc84aabcc01a0
DevExpressExampleBot <fakeEmail>
2023-09-22 21:56:31 +0400

    Source auto update [skip ci]

--- VB/XtraGauges_CustomDraw/Form1.Designer.vb
+++ VB/XtraGauges_CustomDraw/Form1.Designer.vb
@@ -1,133 +1,135 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Namespace XtraGauges_CustomDraw
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.gaugeControl1 = New DevExpress.XtraGauges.Win.GaugeControl()
-			Me.circularGauge1 = New DevExpress.XtraGauges.Win.Gauges.Circular.CircularGauge()
-			Me.arcScaleBackgroundLayerComponent1 = New DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleBackgroundLayerComponent()
-			Me.arcScaleComponent1 = New DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleComponent()
-			Me.arcScaleNeedleComponent1 = New DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleNeedleComponent()
-			Me.checkEdit1 = New DevExpress.XtraEditors.CheckEdit()
-			CType(Me.circularGauge1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.arcScaleBackgroundLayerComponent1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.arcScaleComponent1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.arcScaleNeedleComponent1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.checkEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' gaugeControl1
-			' 
-			Me.gaugeControl1.Dock = System.Windows.Forms.DockStyle.Top
-			Me.gaugeControl1.Gauges.AddRange(New DevExpress.XtraGauges.Base.IGauge() { Me.circularGauge1})
-			Me.gaugeControl1.Location = New System.Drawing.Point(0, 0)
-			Me.gaugeControl1.Name = "gaugeControl1"
-			Me.gaugeControl1.Size = New System.Drawing.Size(300, 260)
-			Me.gaugeControl1.TabIndex = 0
-			' 
-			' circularGauge1
-			' 
-			Me.circularGauge1.BackgroundLayers.AddRange(New DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleBackgroundLayerComponent() { Me.arcScaleBackgroundLayerComponent1})
-			Me.circularGauge1.Bounds = New System.Drawing.Rectangle(6, 6, 288, 248)
-			Me.circularGauge1.Name = "circularGauge1"
-			Me.circularGauge1.Needles.AddRange(New DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleNeedleComponent() { Me.arcScaleNeedleComponent1})
-			Me.circularGauge1.Scales.AddRange(New DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleComponent() { Me.arcScaleComponent1})
-			' 
-			' arcScaleBackgroundLayerComponent1
-			' 
-			Me.arcScaleBackgroundLayerComponent1.ArcScale = Me.arcScaleComponent1
-			Me.arcScaleBackgroundLayerComponent1.Name = "bg1"
-			Me.arcScaleBackgroundLayerComponent1.ShapeType = DevExpress.XtraGauges.Core.Model.BackgroundLayerShapeType.CircularFull_Style1
-			Me.arcScaleBackgroundLayerComponent1.ZOrder = 1000
-'			Me.arcScaleBackgroundLayerComponent1.CustomDrawElement += New DevExpress.XtraGauges.Core.Primitive.CustomDrawElementEventHandler(Me.arcScaleBackgroundLayerComponent1_CustomDrawElement);
-			' 
-			' arcScaleComponent1
-			' 
-			Me.arcScaleComponent1.AppearanceTickmarkText.TextBrush = New DevExpress.XtraGauges.Core.Drawing.SolidBrushObject("Color:Black")
-			Me.arcScaleComponent1.Center = New DevExpress.XtraGauges.Core.Base.PointF2D(125F, 125F)
-			Me.arcScaleComponent1.EndAngle = 60F
-			Me.arcScaleComponent1.MajorTickmark.FormatString = "{0:F0}"
-			Me.arcScaleComponent1.MajorTickmark.ShapeType = DevExpress.XtraGauges.Core.Model.TickmarkShapeType.Circular_Style7_2
-			Me.arcScaleComponent1.MajorTickmark.TextOffset = 22F
-			Me.arcScaleComponent1.MajorTickmark.TextOrientation = DevExpress.XtraGauges.Core.Model.LabelOrientation.LeftToRight
-			Me.arcScaleComponent1.MaxValue = 100F
-			Me.arcScaleComponent1.MinorTickCount = 4
-			Me.arcScaleComponent1.MinorTickmark.ShapeType = DevExpress.XtraGauges.Core.Model.TickmarkShapeType.Circular_Style7_1
-			Me.arcScaleComponent1.Name = "scale1"
-			Me.arcScaleComponent1.RadiusX = 83F
-			Me.arcScaleComponent1.RadiusY = 83F
-			Me.arcScaleComponent1.StartAngle = -240F
-			Me.arcScaleComponent1.Value = 40F
-'			Me.arcScaleComponent1.CustomDrawElement += New DevExpress.XtraGauges.Core.Primitive.CustomDrawElementEventHandler(Me.arcScaleComponent1_CustomDrawElement);
-			' 
-			' arcScaleNeedleComponent1
-			' 
-			Me.arcScaleNeedleComponent1.ArcScale = Me.arcScaleComponent1
-			Me.arcScaleNeedleComponent1.EndOffset = -25F
-			Me.arcScaleNeedleComponent1.Name = "needle1"
-			Me.arcScaleNeedleComponent1.ShapeType = DevExpress.XtraGauges.Core.Model.NeedleShapeType.CircularFull_Style7
-			Me.arcScaleNeedleComponent1.StartOffset = -21F
-			Me.arcScaleNeedleComponent1.ZOrder = -50
-'			Me.arcScaleNeedleComponent1.CustomDrawElement += New DevExpress.XtraGauges.Core.Primitive.CustomDrawElementEventHandler(Me.arcScaleNeedleComponent1_CustomDrawElement);
-			' 
-			' checkEdit1
-			' 
-			Me.checkEdit1.Location = New System.Drawing.Point(76, 264)
-			Me.checkEdit1.Name = "checkEdit1"
-			Me.checkEdit1.Properties.Caption = "Handle CustomDraw event"
-			Me.checkEdit1.Size = New System.Drawing.Size(157, 19)
-			Me.checkEdit1.TabIndex = 1
-'			Me.checkEdit1.CheckStateChanged += New System.EventHandler(Me.checkEdit1_CheckStateChanged);
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(300, 295)
-			Me.Controls.Add(Me.checkEdit1)
-			Me.Controls.Add(Me.gaugeControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-'			Me.Load += New System.EventHandler(Me.Form1_Load);
-			CType(Me.circularGauge1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.arcScaleBackgroundLayerComponent1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.arcScaleComponent1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.arcScaleNeedleComponent1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.checkEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+'#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.gaugeControl1 = New DevExpress.XtraGauges.Win.GaugeControl()
+            Me.circularGauge1 = New DevExpress.XtraGauges.Win.Gauges.Circular.CircularGauge()
+            Me.arcScaleBackgroundLayerComponent1 = New DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleBackgroundLayerComponent()
+            Me.arcScaleComponent1 = New DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleComponent()
+            Me.arcScaleNeedleComponent1 = New DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleNeedleComponent()
+            Me.checkEdit1 = New DevExpress.XtraEditors.CheckEdit()
+            CType((Me.circularGauge1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.arcScaleBackgroundLayerComponent1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.arcScaleComponent1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.arcScaleNeedleComponent1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.checkEdit1.Properties), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' gaugeControl1
+            ' 
+            Me.gaugeControl1.Dock = System.Windows.Forms.DockStyle.Top
+            Me.gaugeControl1.Gauges.AddRange(New DevExpress.XtraGauges.Base.IGauge() {Me.circularGauge1})
+            Me.gaugeControl1.Location = New System.Drawing.Point(0, 0)
+            Me.gaugeControl1.Name = "gaugeControl1"
+            Me.gaugeControl1.Size = New System.Drawing.Size(300, 260)
+            Me.gaugeControl1.TabIndex = 0
+            ' 
+            ' circularGauge1
+            ' 
+            Me.circularGauge1.BackgroundLayers.AddRange(New DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleBackgroundLayerComponent() {Me.arcScaleBackgroundLayerComponent1})
+            Me.circularGauge1.Bounds = New System.Drawing.Rectangle(6, 6, 288, 248)
+            Me.circularGauge1.Name = "circularGauge1"
+            Me.circularGauge1.Needles.AddRange(New DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleNeedleComponent() {Me.arcScaleNeedleComponent1})
+            Me.circularGauge1.Scales.AddRange(New DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleComponent() {Me.arcScaleComponent1})
+            ' 
+            ' arcScaleBackgroundLayerComponent1
+            ' 
+            Me.arcScaleBackgroundLayerComponent1.ArcScale = Me.arcScaleComponent1
+            Me.arcScaleBackgroundLayerComponent1.Name = "bg1"
+            Me.arcScaleBackgroundLayerComponent1.ShapeType = DevExpress.XtraGauges.Core.Model.BackgroundLayerShapeType.CircularFull_Style1
+            Me.arcScaleBackgroundLayerComponent1.ZOrder = 1000
+            AddHandler Me.arcScaleBackgroundLayerComponent1.CustomDrawElement, New DevExpress.XtraGauges.Core.Primitive.CustomDrawElementEventHandler(AddressOf Me.arcScaleBackgroundLayerComponent1_CustomDrawElement)
+            ' 
+            ' arcScaleComponent1
+            ' 
+            Me.arcScaleComponent1.AppearanceTickmarkText.TextBrush = New DevExpress.XtraGauges.Core.Drawing.SolidBrushObject("Color:Black")
+            Me.arcScaleComponent1.Center = New DevExpress.XtraGauges.Core.Base.PointF2D(125F, 125F)
+            Me.arcScaleComponent1.EndAngle = 60F
+            Me.arcScaleComponent1.MajorTickmark.FormatString = "{0:F0}"
+            Me.arcScaleComponent1.MajorTickmark.ShapeType = DevExpress.XtraGauges.Core.Model.TickmarkShapeType.Circular_Style7_2
+            Me.arcScaleComponent1.MajorTickmark.TextOffset = 22F
+            Me.arcScaleComponent1.MajorTickmark.TextOrientation = DevExpress.XtraGauges.Core.Model.LabelOrientation.LeftToRight
+            Me.arcScaleComponent1.MaxValue = 100F
+            Me.arcScaleComponent1.MinorTickCount = 4
+            Me.arcScaleComponent1.MinorTickmark.ShapeType = DevExpress.XtraGauges.Core.Model.TickmarkShapeType.Circular_Style7_1
+            Me.arcScaleComponent1.Name = "scale1"
+            Me.arcScaleComponent1.RadiusX = 83F
+            Me.arcScaleComponent1.RadiusY = 83F
+            Me.arcScaleComponent1.StartAngle = -240F
+            Me.arcScaleComponent1.Value = 40F
+            AddHandler Me.arcScaleComponent1.CustomDrawElement, New DevExpress.XtraGauges.Core.Primitive.CustomDrawElementEventHandler(AddressOf Me.arcScaleComponent1_CustomDrawElement)
+            ' 
+            ' arcScaleNeedleComponent1
+            ' 
+            Me.arcScaleNeedleComponent1.ArcScale = Me.arcScaleComponent1
+            Me.arcScaleNeedleComponent1.EndOffset = -25F
+            Me.arcScaleNeedleComponent1.Name = "needle1"
+            Me.arcScaleNeedleComponent1.ShapeType = DevExpress.XtraGauges.Core.Model.NeedleShapeType.CircularFull_Style7
+            Me.arcScaleNeedleComponent1.StartOffset = -21F
+            Me.arcScaleNeedleComponent1.ZOrder = -50
+            AddHandler Me.arcScaleNeedleComponent1.CustomDrawElement, New DevExpress.XtraGauges.Core.Primitive.CustomDrawElementEventHandler(AddressOf Me.arcScaleNeedleComponent1_CustomDrawElement)
+            ' 
+            ' checkEdit1
+            ' 
+            Me.checkEdit1.Location = New System.Drawing.Point(76, 264)
+            Me.checkEdit1.Name = "checkEdit1"
+            Me.checkEdit1.Properties.Caption = "Handle CustomDraw event"
+            Me.checkEdit1.Size = New System.Drawing.Size(157, 19)
+            Me.checkEdit1.TabIndex = 1
+            AddHandler Me.checkEdit1.CheckStateChanged, New System.EventHandler(AddressOf Me.checkEdit1_CheckStateChanged)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(300, 295)
+            Me.Controls.Add(Me.checkEdit1)
+            Me.Controls.Add(Me.gaugeControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.circularGauge1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.arcScaleBackgroundLayerComponent1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.arcScaleComponent1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.arcScaleNeedleComponent1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.checkEdit1.Properties), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private gaugeControl1 As DevExpress.XtraGauges.Win.GaugeControl
-		Private circularGauge1 As DevExpress.XtraGauges.Win.Gauges.Circular.CircularGauge
-		Private WithEvents arcScaleBackgroundLayerComponent1 As DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleBackgroundLayerComponent
-		Private WithEvents arcScaleComponent1 As DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleComponent
-		Private WithEvents arcScaleNeedleComponent1 As DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleNeedleComponent
-		Private WithEvents checkEdit1 As DevExpress.XtraEditors.CheckEdit
-	End Class
+'#End Region
+        Private gaugeControl1 As DevExpress.XtraGauges.Win.GaugeControl
+
+        Private circularGauge1 As DevExpress.XtraGauges.Win.Gauges.Circular.CircularGauge
+
+        Private arcScaleBackgroundLayerComponent1 As DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleBackgroundLayerComponent
+
+        Private arcScaleComponent1 As DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleComponent
+
+        Private arcScaleNeedleComponent1 As DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleNeedleComponent
+
+        Private checkEdit1 As DevExpress.XtraEditors.CheckEdit
+    End Class
 End Namespace
-
